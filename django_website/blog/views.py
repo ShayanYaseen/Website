92783532607741d12148ae442b15c5ba538e62df
ShayanYaseen <shayan.yaseen1@gmail.com>
2019-06-24 01:15:39 +0530

    user registration (wip)

--- django_website/blog/views.py
+++ django_website/blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-posts = [
-    {
-        'author': 'Shayan',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': '24 May 2019'
-    },
-    {
-        'author': 'Shayan',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': '25 May 2019'
-    },
-]
-
+from .models import Post
 
 def home(request):
     context ={
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
